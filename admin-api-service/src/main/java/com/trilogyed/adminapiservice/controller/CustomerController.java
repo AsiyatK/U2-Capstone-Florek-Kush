0db83145e8b29fb381fe5da5018e965eb7544c70
Asiyat Kush <Alicia@AMINATs-MacBook-Pro.local>
2019-10-10 13:57:11 -0400

    admin service models and security

--- admin-api-service/src/main/java/com/trilogyed/adminapiservice/controller/CustomerController.java
+++ admin-api-service/src/main/java/com/trilogyed/adminapiservice/controller/CustomerController.java
@@ -1,14 +1,14 @@
 package com.trilogyed.adminapiservice.controller;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
 
-
-    ///please work now!!
 }
---
+++ admin-api-service/src/main/java/com/trilogyed/adminapiservice/viewModel/InventoryViewModel.java
@@ -0,0 +1,58 @@
+package com.trilogyed.adminapiservice.viewModel;
+
+import java.util.Objects;
+
+public class InventoryViewModel {
+
+    private int inventoryId;
+    private int productId;
+    private int quantity;
+
+    public int getInventoryId() {
+        return inventoryId;
+    }
+
+    public void setInventoryId(int inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InventoryViewModel that = (InventoryViewModel) o;
+        return getInventoryId() == that.getInventoryId() &&
+                getProductId() == that.getProductId() &&
+                getQuantity() == that.getQuantity();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getInventoryId(), getProductId(), getQuantity());
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryViewModel{" +
+                "inventoryId=" + inventoryId +
+                ", productId=" + productId +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
